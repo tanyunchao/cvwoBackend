586684fe697681b58653afdbd71c2a6fa0039c22
yunchao <tanyunchao@gmail.com>
2022-12-23 17:43:08 +0800

    added votes and sorted list to homepage

--- src/TopThree.js
+++ src/TopThree.js
@@ -2,23 +2,42 @@
 const TopThree = ({posts}) => {
     posts = Array.from(posts);
 
+    const topN = (arr, n) => {
+        if(n > arr.length){
+           return false;
+        }
+        return arr
+        .slice()
+        .sort((a, b) => {
+           return b.votes - a.votes
+        })
+        .slice(0, n);
+    };
+
+    //filters for top 3 highest posts 
+    const filtered = topN(posts, 3);
+
     return (  
         <div className="topThreePosts list-group" >
-            {/* stop gap measure for now, replace map with filter for actual filtering of posts */}
-            {posts.map(post => (
-                // <div className="list-group">
-                //     <h1>{post.title}</h1>
-                //     <h2>{post.body}</h2>
-                //     <h3>Submitted by [author name by right] on {post.date}</h3>
-                // </div>
-
-                <a href="/pages:id" className="list-group-item list-group-item-action active topThreePost" aria-current="true">
+            {filtered.map(post => (
+                <a key={post.id} href="/pages:id" className="list-group-item list-group-item-action active topThreePost" aria-current="true">
                     <div className="d-flex w-100 justify-content-between ">
                         <h5 className="mb-1">{post.title}</h5>
-                        <small>Submitted by [author name by right] on {post.date}</small>
+                        <small>Submitted by {post.authorId} on {post.date}</small>
                     </div>
-                    <p className="mb-1">{post.body}</p>
-                    <small>NIL</small>
+                    <div style={{display: "flex"}}>
+                        <p className="mb-1">{post.body}</p>
+                        <div className="votingButtons">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-up-fill" viewBox="0 0 16 16">
+                                <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+                            </svg>
+                            <p className="votebox">{post.votes}</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                            </svg>
+                        </div>
+                    </div>
+                    <small>TODO: add in how many comments</small>
                 </a>
             ))}
 
