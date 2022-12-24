7a4a8a3a20224061f54fc92ce92cb7eaaf6a2963
yunchao <tanyunchao@gmail.com>
2022-12-24 17:01:55 +0800

    updated navbar options

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -14,8 +14,8 @@ const NavigationBar = () => {
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/threads">Threads</Nav.Link>
                     <NavDropdown title="Others" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">FAQs</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.1">Community Rules</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Feedback Form</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#action/3.4">
                         Separated link
