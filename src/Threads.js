5885f4a30871b2f74ca2e5b877773986aaa9b646
yunchao <tanyunchao@gmail.com>
2022-12-26 16:10:48 +0800

    deleted unnecessary comments

--- src/Threads.js
+++ src/Threads.js
@@ -47,26 +47,7 @@ const Threads = () => {
                         </Accordion.Body>
                     </Accordion.Item>
                 ))}
-                
-                
-                {/* <Accordion.Item eventKey="0">
-                    <Accordion.Header>SOC Academic</Accordion.Header>
-                    <Accordion.Body>
-                        <h3>testing values</h3>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>Town Square</Accordion.Header>
-                    <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                    </Accordion.Body>
-                </Accordion.Item> */}
+                                
             </Accordion>
         </div>
 
