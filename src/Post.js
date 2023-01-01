d5830030f3fbd0690432b06e9f9bd114486515ab
yunchao <tanyunchao@gmail.com>
2023-01-01 22:25:20 +0800

    updated styling for comments

--- src/Post.js
+++ src/Post.js
@@ -58,7 +58,8 @@ const Post = () => {
                                     <p>forum_user</p>
                                 </div>
                                 <div className="commentBody">
-                                    <p style={{borderStyle: "solid"}}>This is genuinely unexpected. How could this have happened?!</p>
+                                    <p className="timestamp">Posted 10hrs ago</p>
+                                    <p >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                                 </div>
                             </div>
                         </div>
