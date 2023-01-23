f9843a2b08880af0f31b42f56aa65db01e0a4826
yunchao <tanyunchao@gmail.com>
2023-01-24 01:53:00 +0800

    intiated indiv threads component

---
+++ src/ThreadPage.js
@@ -0,0 +1,12 @@
+const ThreadPage = () => {
+
+    
+
+    return ( 
+        <div>
+            <p>this is the page for all the posts in a thread</p>
+        </div>
+     );
+}
+ 
+export default ThreadPage;
